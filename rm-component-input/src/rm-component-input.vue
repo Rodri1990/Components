d4424de31a9d468850b335d7c13a8d37d0d43e34
Rodri1990 <rmontero.web@gmail.com>
2022-05-29 17:28:15 -0300

    update name component comment

--- rm-component-input/src/rm-component-input.vue
+++ rm-component-input/src/rm-component-input.vue
@@ -2,7 +2,7 @@
   import {defineComponent} from 'vue';
 
   export default /*#__PURE__*/ defineComponent({
-    name: 'RmComponentInput', // vue component name
+    name: 'RmComponentInput', // name of component
     data() {
       return {
         label: this.label,
